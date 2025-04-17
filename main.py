6e44c59b46e6b2d8d67f1717ce2f6f87b9b41e2f
hitesh7894 <108324045+hitesh7894@users.noreply.github.com>
2025-04-17 21:53:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
@@ -11,10 +12,6 @@ def webhook():
     data = request.json
     print(data)
     return '', 200
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
-import os
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8000))
