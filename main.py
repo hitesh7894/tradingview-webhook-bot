971e6078fdd6ef91765f97b8093b05d2267333b8
hitesh7894 <108324045+hitesh7894@users.noreply.github.com>
2025-04-17 21:12:56 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Webhook is live!'
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.json
+    print("Webhook received:", data)
+    return jsonify({'status': 'success', 'data': data}), 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
